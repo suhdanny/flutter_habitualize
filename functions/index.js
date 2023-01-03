6582ec859384d63b66aa61dfd3f69f4943c33e4a
suhdanny <suhdong21@gmail.com>
2023-01-03 11:43:24 -0800

    Add scheduled cloud function that resets completed field to false every midnight

---
+++ functions/index.js
@@ -0,0 +1,26 @@
+const functions = require("firebase-functions");
+
+const admin = require("firebase-admin");
+admin.initializeApp();
+
+exports.resetCompleted = functions.pubsub.schedule("0 0 * * *").onRun((_) => {
+  const db = admin.firestore();
+
+  const usersRef = db.collection("users");
+
+  usersRef.get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+      const habitsRef = usersRef.doc(doc.id).collection("habits");
+
+      habitsRef.get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          habitsRef.doc(doc.id).update({
+            completed: false,
+          });
+        });
+      });
+    });
+  });
+
+  console.log("Reset Completed", new Date().toLocaleString());
+});
