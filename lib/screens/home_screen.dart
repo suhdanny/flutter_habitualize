880b7abcd24dde835005bf880694ba3ee06e14a8
suhdanny <suhdong21@gmail.com>
2023-01-06 00:35:53 -0500

    Delete ListView comment

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -85,46 +85,6 @@ class HomeScreen extends StatelessWidget {
                     .snapshots(),
               ),
             ),
-            // Expanded(
-            //   child: StreamBuilder(
-            //     builder: (ctx, snapshot) {
-            //       if (snapshot.data == null) {
-            //         return const Center(
-            //           child: CircularProgressIndicator(),
-            //         );
-            //       }
-            //       return ListView.builder(
-            //         itemBuilder: (ctx, idx) {
-            //           Map<String, dynamic> data =
-            //               snapshot.data!.docs[idx].data();
-            //           int codePoint = int.parse(
-            //               data['icon'].split('U+')[1].split(')')[0],
-            //               radix: 16);
-            //           IconData icon =
-            //               IconData(codePoint, fontFamily: "MaterialIcons");
-            //           Color iconColor =
-            //               Color(int.parse(data['iconColor'], radix: 16));
-            //           return HabitListTile(
-            //             docId: snapshot.data!.docs[idx].id,
-            //             icon: icon,
-            //             iconColor: iconColor,
-            //             title: data['title'],
-            //             count: data['count'],
-            //             countUnit: data['countUnit'],
-            //             dayCount: data['dayCount'],
-            //             duration: data['duration'],
-            //             streaks: data['streaks'],
-            //             completed: data['completed'],
-            //           );
-            //         },
-            //         itemCount: snapshot.data!.docs.length,
-            //       );
-            //     },
-            //     stream: FirebaseFirestore.instance
-            //         .collection('/users/$userUid/habits')
-            //         .snapshots(),
-            //   ),
-            // ),
           ],
         ),
       ),
