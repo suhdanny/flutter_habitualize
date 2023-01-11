4b1ce8c72affc48c844840148afd88a515c98ae2
suhdanny <suhdong21@gmail.com>
2023-01-11 00:06:56 -0500

    Fix heatmap so that it only reflects completion based on habits that are tracked on a certain date

--- lib/utils/create_new_timeline.dart
+++ lib/utils/create_new_timeline.dart
@@ -1,20 +1,34 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:intl/intl.dart';
-
-final userUid = FirebaseAuth.instance.currentUser!.uid;
+import '../utils/get_weekday_string.dart';
 
 void createNewTimeline(date) async {
+  final userUid = FirebaseAuth.instance.currentUser!.uid;
+
   final snapshot = await FirebaseFirestore.instance
       .collection('users/$userUid/habits')
       .get();
   for (var doc in snapshot.docs) {
     final data = doc.data();
+
+    bool isTracked = false;
+    if (data['duration'] == 'day') {
+      final dailyTracks = Map<String, bool>.from(data['dailyTracks']);
+      if (dailyTracks[getWeekdayString(date)] == true) {
+        isTracked = true;
+      }
+    } else {
+      final weekTracks = data['weekTrack'];
+      if (weekTracks == getWeekdayString(date)) {
+        isTracked = true;
+      }
+    }
+
     String selectedDate = DateFormat('yyyy-MM-dd').format(date);
     Map<String, dynamic> timeline = data['timeline'];
 
-    if (!timeline.containsKey(selectedDate)) {
+    if (!timeline.containsKey(selectedDate) && isTracked) {
       await doc.reference.update({
         "timeline.$selectedDate": {
           "completed": false,
